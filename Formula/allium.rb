92dcfd6c6c633c35a66f6c83423df5a05cbfd924
Henry Garner <hga@juxt.pro>
2026-02-26 14:02:25 +0000

    Update to v0.1.4

--- Formula/allium.rb
+++ Formula/allium.rb
@@ -1,8 +1,8 @@
 class Allium < Formula
   desc "Checker and parser for the Allium specification language"
   homepage "https://github.com/juxt/allium-tools"
-  url "https://github.com/juxt/allium-tools/archive/refs/tags/v0.1.3.tar.gz"
-  sha256 "27d3f9c6974fec78a754a8030987b7b08092aca807f5d1023426411eede4e778"
+  url "https://github.com/juxt/allium-tools/archive/refs/tags/v0.1.4.tar.gz"
+  sha256 "87bb20e7dc550a61b47efca4bf410bf0c1b73b9471de2169077cf5ec9912e6d4"
   license "MIT"
 
   depends_on "rust" => :build
