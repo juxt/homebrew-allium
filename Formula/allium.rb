e0820b1572410da736e5963039692fef3c1451cd
Henry Garner <hga@juxt.pro>
2026-02-28 16:47:19 +0000

    Update allium to 0.1.8

--- Formula/allium.rb
+++ Formula/allium.rb
@@ -1,28 +1,28 @@
 class Allium < Formula
   desc "Checker and parser for the Allium specification language"
   homepage "https://github.com/juxt/allium-tools"
-  version "0.1.7"
+  version "0.1.8"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/juxt/allium-tools/releases/download/v#{version}/allium-aarch64-apple-darwin.tar.gz"
-      sha256 "d8ec47c6ca94e89348a4b452c7914c9f27f22f7973e1f02752e4d97439e379ba"
+      sha256 "213c4c2eaf9400b3768de6feadb874484cbd02c1fa66f693fb45148a87ee41e4"
     end
     if Hardware::CPU.intel?
       url "https://github.com/juxt/allium-tools/releases/download/v#{version}/allium-x86_64-apple-darwin.tar.gz"
-      sha256 "64e3a61bd1640123ac5f65f7bcf055de4a12b3acc2ae63674e0ab16120ae7d8f"
+      sha256 "ea63b41736071b273ccc97c20a8605e2066da52685a71356a3c49d816c1ad9e9"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
       url "https://github.com/juxt/allium-tools/releases/download/v#{version}/allium-aarch64-unknown-linux-gnu.tar.gz"
-      sha256 "59894a23f5fa618bb347eb254ea1193762221ce82e29a9cdc383a5dcf8d48d2d"
+      sha256 "52ba4342aef63e6dde3904611142ccd03214925317e5f9fb5779ddcfa469d587"
     end
     if Hardware::CPU.intel?
       url "https://github.com/juxt/allium-tools/releases/download/v#{version}/allium-x86_64-unknown-linux-gnu.tar.gz"
-      sha256 "121a0c69bd2038b0958d7ca07fff50abc754a60f9edf3bb8009efc7816dd28e3"
+      sha256 "0fcd7b61f1816c2cfeaaac7eeb89f02a188fa492160bdde1667e3c627ecb6ff0"
     end
   end
 
